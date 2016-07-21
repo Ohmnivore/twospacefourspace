4ad9532b441a06e730ccea6ca02a08ed78871633
Mark Beiline <mbeiline@explorance.com>
2016-07-21 15:31:47 -0400

    selection now handled

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # twospacefourspace
-Converts current working document between two-space indentation and four-space indentation.
+This is a four-space and two-space indentation interconverter.
 This is a preview because I still need to use it a few times and see if there are some weird cases I need to account for.
 
 ## Features
 * `Ctrl-Shift-P` or `F1` to open the command menu, then type either of the following:
 * `Two to four (indentation)`
 * `Four to two (indentation)`
+* If there is no active selection, twospacefourspace will act on the entire file
 
 ## Keyboard Shortcuts
 Example of addition to keyboard shortcuts config file (open using the `File` menu or by running the `Preferences: Open Keyboard Shortcuts` command):
@@ -23,10 +24,12 @@ Example of addition to keyboard shortcuts config file (open using the `File` men
 ```
 
 ## Planned Features
-* If there is a selection(s), act on it instead of the entire file
-* Throw warning dialog when indentation isn't two or four spaces and can't properly convert
+* Throw warning dialog when indentation isn't two or four spaces and can't properly convert without 'losing' whitespace
 * Deal with edge cases if/when I encounter them
 
 ## Release Notes
+### 0.2.0
+Now handles selection
+
 ### 0.0.1
 Initial release
--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,27 @@
 import * as vscode from 'vscode';
 import { EOL } from 'os';
 
+// Dirty hack to ignore leftover selection after twoToFour
+// Don't have much choice
+let usedSelection = false;
+let useSelection = true;
+let oldAnchor;
+let oldWasEmpty = true;
+function onSelectChange(e: vscode.TextEditorSelectionChangeEvent) {
+    useSelection = true;
+    if (usedSelection) {
+        if (vscode.window.activeTextEditor.selection.isEmpty)
+            useSelection = false;
+    }
+    else {
+        if (oldWasEmpty && vscode.window.activeTextEditor.selection.anchor.isEqual(oldAnchor))
+            useSelection = false;
+    }
+
+    oldAnchor = vscode.window.activeTextEditor.selection.anchor;
+    oldWasEmpty = vscode.window.activeTextEditor.selection.isEmpty;
+}
+
 function twoToFour(s: string, spacecount: number): string {
     for (let i = 0; i < spacecount; i ++)
         s = ' ' + s;
@@ -14,11 +35,31 @@ function fourToTwo(s: string, spacecount: number): string {
 
 function nukeIndent(indentFunction: (string, number) => string) {
     const editor = vscode.window.activeTextEditor;
-    const oldtext = editor.document.getText();
+
+    // Handling selection
+    let oldtext: string;
+    let firstline: vscode.TextLine;
+    let lastline: vscode.TextLine;
+    if (!useSelection || editor.selection == null || editor.selection.isEmpty) {
+        if (editor.document.lineCount <= 0)
+            return;
+        firstline = editor.document.lineAt(0);
+        lastline = editor.document.lineAt(editor.document.lineCount - 1);
+        oldtext = editor.document.getText();
+        usedSelection = false;
+    }
+    else {
+        firstline = editor.document.lineAt(editor.selection.start.line);
+        lastline = editor.document.lineAt(editor.selection.end.line);
+        oldtext = editor.document.getText(new vscode.Range(firstline.lineNumber, 0, lastline.lineNumber, lastline.range.end.character));
+        usedSelection = true;
+    }
+    oldWasEmpty = vscode.window.activeTextEditor.selection.isEmpty;
+    oldAnchor = vscode.window.activeTextEditor.selection.anchor;
 
     // Prepare for nuking indentation
     const lines = [];
-    for (let i = 0; i < editor.document.lineCount; i++)
+    for (let i = firstline.lineNumber; i <= lastline.lineNumber; i++)
         lines.push(editor.document.lineAt(i).text);
 
     for (let i = 0; i < lines.length; i++) {
@@ -38,20 +79,21 @@ function nukeIndent(indentFunction: (string, number) => string) {
 
     if (newtext !== oldtext) {
         // Apply edit
-        editor.edit((edit: vscode.TextEditorEdit) => {
-            const lastline = editor.document.lineAt(editor.document.lineCount - 1);
-            edit.replace(new vscode.Range(0, 0, lastline.lineNumber, lastline.range.end.character), newtext);
-        });
+        editor.edit((edit: vscode.TextEditorEdit) =>
+            edit.replace(new vscode.Range(firstline.lineNumber, 0, lastline.lineNumber, lastline.range.end.character), newtext)
+        );
     }
 }
 
 export function activate(context: vscode.ExtensionContext) {
-    context.subscriptions.push(vscode.commands.registerCommand('twospacefourspace.twoToFour', () => {
-        nukeIndent(twoToFour);
-    }));
-    context.subscriptions.push(vscode.commands.registerCommand('twospacefourspace.fourToTwo', () => {
-        nukeIndent(fourToTwo);
-    }));
+    context.subscriptions.push(vscode.commands.registerCommand('twospacefourspace.twoToFour', () =>
+        nukeIndent(twoToFour)
+    ));
+    context.subscriptions.push(vscode.commands.registerCommand('twospacefourspace.fourToTwo', () =>
+        nukeIndent(fourToTwo)
+    ));
+
+    vscode.window.onDidChangeTextEditorSelection(onSelectChange);
 }
 
 export function deactivate() {}
